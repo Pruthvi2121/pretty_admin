56c8330f954e82f611873371f704182dff7cb4fc
Pruthvi2121 <chokake.pruthvi@gmail.com>
2025-04-25 14:14:42 +0530

    version 1.4

--- pretty_admin/setup.py
+++ pretty_admin/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pretty_admin',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     include_package_data=True,
     description='A Tailwind-based Django admin UI skin',
