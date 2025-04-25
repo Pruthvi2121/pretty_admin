a8206091d0f337a9cef93ca7d1ff0fad33c905cd
Pruthvi2121 <chokake.pruthvi@gmail.com>
2025-04-25 14:46:13 +0530

    doc updated

--- pretty_admin/setup.py
+++ pretty_admin/setup.py
@@ -1,14 +1,27 @@
 from setuptools import setup, find_packages
+import os
+# Read the README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 
 setup(
     name='pretty_admin',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     include_package_data=True,
     description='A Tailwind-based Django admin UI skin',
+    long_description=long_description,
     author='Pruthviraj Chokake',
+    author_email='chokake.pruthvi@gmail.com',
+    license='MIT',
     install_requires=[
         'Django>=3.2',
         'setuptools'
     ],
+    python_requires='>=3.6',
+    project_urls={
+        'Homepage': 'https://github.com/Pruthvi2121/pretty_admin',  
+        'Bug Tracker': 'https://github.com/Pruthvi2121/pretty_admin/issues',
+    },
 )
